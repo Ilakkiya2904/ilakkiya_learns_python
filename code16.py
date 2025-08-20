3a058687cbfbac679d24035948e9d380f733a47b
Ilakkiya2904 <ilakkiya294@gmail.com>
2025-08-20 22:14:56 +0530

    Create code16.py
    Given a number n, write a program to print a diamond of ((2*n)-1)
    rows using stars (*)
    
      *
     * *
    * * *
     * *
      *

---
+++ code16.py
@@ -0,0 +1,9 @@
+n=int(input())
+for i in range(1,n+1):
+    spaces=(" ")*(n-i)
+    stars=("* ")*i 
+    print(spaces+stars)
+for i in range(1,n):
+    spaces=(" ")*i 
+    stars=("* ")*(n-i)
+    print(spaces+stars)
